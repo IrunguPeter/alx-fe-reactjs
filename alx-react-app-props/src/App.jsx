6c8da51a800c2bce7151b0e6d9e5016f3625c1e1
Peter Irungu <Irungupeter204@gmail.com>
2026-01-15 11:55:00 +0300

    Refactore prop drilling to use context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import ProfilePage from './components/ProfilePage';
+import UserContext from './context/UserContext';
+
+function App() {
+  const userData = {
+    name: "Alice",
+    age: "25",
+    bio: "Loves hiking and photography"
+  };
+
+  return (
+    <div className="App">
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserInfo from './UserInfo';
+
+const ProfilePage = () => {
+  return <UserInfo />;
+};
+
+export default ProfilePage;

371eed6432509a3a64d6805c93aea70ffb2245b7
Peter Irungu <irungupeter204@gmail.com>
2026-01-24 20:59:33 +0300

    fixed even more errors

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
@@ -8,24 +8,26 @@ import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Recipe Sharing Application</h1>
-      <SearchBar />
-      <Routes>
-        <Route path="/" element={
-          <>
-            <AddRecipeForm />
-            <hr />
-            <RecipeList />
-            <hr />
-            <FavoritesList />
-            <hr />
-            <RecommendationsList />
-          </>
-        } />
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <Router>
+      <div style={{ padding: '20px' }}>
+        <h1>Recipe Sharing Application</h1>
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <AddRecipeForm />
+              <hr />
+              <RecipeList />
+              <hr />
+              <FavoritesList />
+              <hr />
+              <RecommendationsList />
+            </>
+          } />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

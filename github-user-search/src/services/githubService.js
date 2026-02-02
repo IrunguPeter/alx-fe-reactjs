ef87b2e14b1be2fa4c58f7ba85fdce1dd57d4669
Peter Irungu <irungupeter204@gmail.com>
2026-02-02 09:35:08 +0300

    Added some more files

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,1 +1,13 @@
-//services
+import axios from 'axios';
+
+const GITHUB_URL = 'https://api.github.com';
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_URL}/users/${username}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    throw error;
+  }
+};

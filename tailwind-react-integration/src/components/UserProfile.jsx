63541923fd8e2755dc4939bef873f4d607167bbd
Peter Irungu <irungupeter204@gmail.com>
2026-02-08 15:10:46 +0300

    fix: Corrected a typo in the container in UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg md:p-8 md:max-w-sm">
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg sm:p-4 shadow-lg md:p-8 md:max-w-sm">
       <img src="https://via.placeholder.com/150" alt="User" 
       className="rounded-full w-36 h-36 mx-auto mb-4 object-cover border-4 sm:w-24 sm:h-24 md:w-36 md:h-36 border-white shadow-sm"/>
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>

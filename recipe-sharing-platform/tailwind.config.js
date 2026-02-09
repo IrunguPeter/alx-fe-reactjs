a79932b225a980a52b4e0a297c38afe4cf3d0f7f
Peter Irungu <Irungupeter204@gmail.com>
2026-02-09 21:43:16 +0300

    Created a new react project

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./srcjs,/**/*.{jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+

15c622330cc202af0136ea212c96c53343b742e0
Peter Irungu <Irungupeter204@gmail.com>
2026-02-09 21:46:47 +0300

    Modification: Modified tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./srcjs,/**/*.{jsx,ts,tsx}', './public/index.html'],
+  content: ["./src/**/*.{jsx,ts,tsx}", "./public/index.html"],
   darkMode: 'media',
   theme: {
     extend: {},

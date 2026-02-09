37860074e80775ad0804f3a8b60a633e3422b5ab
Peter Irungu <Irungupeter204@gmail.com>
2026-02-09 22:07:46 +0300

    Updates: Modifies ReadME and tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{jsx,ts,tsx}", "./public/index.html"],
-  darkMode: 'media',
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
 }
 
-
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
